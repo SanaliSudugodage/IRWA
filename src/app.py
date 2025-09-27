48f6a7dba7390598cf1e6c5ef0d5dde4a4098298
SanaliSudugodage <IT22557742@my.sliit.lk>
2025-09-27 12:05:17 +0530

    commits added for router

--- src/app.py
+++ src/app.py
@@ -80,7 +80,7 @@ try:
 except Exception as e:
     logger.warning(f"Skipping IR admin router: {e}")
 
-#  Swagger: Bearer auth everywhere 
+#  Swagger: Bearer auth  
 def custom_openapi():
     if app.openapi_schema:
         return app.openapi_schema
