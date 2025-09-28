fd63bd89920ea6269474d738a04bc1a04a07d7a7
Aarabhi Anton Yoganayagam <156441711+aarabhi12@users.noreply.github.com>
2025-09-28 07:30:53 +0530

    Update test_argument_generator.py

--- tests/test_argument_generator.py
+++ tests/test_argument_generator.py
@@ -40,6 +40,6 @@ def test_generate_argument_works_without_stance():
     data = resp.json()
     assert "Position on" in data["thesis"]
 
-########################################################
+
 
 
